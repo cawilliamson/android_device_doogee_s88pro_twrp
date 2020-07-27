2842fb07a8c3c37ef415499c5d04476d2509de5d
Christopher A. Williamson <home@chrisaw.com>
2020-07-27 09:33:19 +0100

    Initial commit (nabbed from omni realme3)

---
+++ README.md
@@ -0,0 +1,1 @@
+These sources are a work in progress to port TWRP to the Doogee S88 Pro - please do not use them yet!
---
+++ omni_RMX1821.mk
@@ -0,0 +1,26 @@
+# Release name
+PRODUCT_RELEASE_NAME := RMX1821
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Realme
+PRODUCT_DEVICE := RMX1821
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_MODEL := RMX1821
+PRODUCT_NAME := omni_RMX1821
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    ro.allow.mock.location=0
